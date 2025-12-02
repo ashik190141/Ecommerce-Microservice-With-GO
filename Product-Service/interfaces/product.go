e468cb24a047860f46d9a21fafdd39e0f02ece13
TN-LAP-0184\TN-99648 <ahsan.mahmud@technonext.com>
2025-12-02 12:59:06 +0600

    add grpc of product service

--- Product-Service/interfaces/product.go
+++ Product-Service/interfaces/product.go
@@ -3,9 +3,9 @@ package interfaces
 import "Product-Service/dto"
 
 type ProductInterface interface {
-	CreateProduct(dto.CreateProductDTO) (bool, string)
+	CreateProduct(dto.CreateProductDTO) bool
 	GetProductByID(id int) (dto.GetProductResponse, string)
-	UpdateProduct(id int) (bool, string)
-	DeleteProduct(id int) (bool, string)
+	UpdateProduct(id int) bool
+	DeleteProduct(id int) bool
 	GetProducts() ([]dto.GetProductResponse, error)
 }
--- Product-Service/repo/repository.go
+++ Product-Service/repo/repository.go
@@ -1,2 +1,40 @@
 package repo
 
+import (
+	"Product-Service/dto"
+	"Product-Service/interfaces"
+	"context"
+	"github.com/jmoiron/sqlx"
+)
+
+type ProductRepository struct {
+	db  *sqlx.DB
+	ctx context.Context
+}
+
+func NewProductRepository(db *sqlx.DB) interfaces.ProductInterface {
+	return &ProductRepository{
+		db:  db,
+		ctx: context.Background(),
+	}
+}
+
+func (r *ProductRepository) CreateProduct(product dto.CreateProductDTO) (bool) {
+	return false
+}
+
+func (r *ProductRepository) GetProductByID(id int) (dto.GetProductResponse, string) {
+	return dto.GetProductResponse{}, "not implemented"
+}
+
+func (r *ProductRepository) UpdateProduct(id int) bool {
+	return false
+}
+
+func (r *ProductRepository) DeleteProduct(id int) bool {
+	return false
+}
+
+func (r *ProductRepository) GetProducts() ([]dto.GetProductResponse, error) {
+	return nil, nil
+}
